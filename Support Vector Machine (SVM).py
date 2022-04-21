706bf714f90e313a6d8cc79d7678f3b7360d8de3
apoorva1720 <99265266+apoorva1720@users.noreply.github.com>
2022-04-21 23:21:16 +0530

    changed ML model

--- Support Vector Machine (SVM).py
+++ Support Vector Machine (SVM).py
@@ -13,7 +13,7 @@ y = dataset.iloc[:, 0].values
 start=time.time()
 # Splitting the dataset into the Training set and Test set
 from sklearn.model_selection import train_test_split, cross_val_score
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.25, random_state = 0)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)
 
 # Feature Scaling
 from sklearn.preprocessing import StandardScaler
@@ -28,7 +28,9 @@ classifier.fit(X_train, y_train)
 from sklearn.model_selection import cross_val_score
 scores = cross_val_score(classifier, X_train, y_train, cv=10)
 print(scores,scores.mean())
+end=time.time()
 
+start1=time.time()
 import joblib
 # Save the model as a pickle in a file
 joblib.dump(classifier, 'SVM.pkl') 
@@ -37,7 +39,7 @@ Test_Classifier = joblib.load('SVM.pkl')
 
 # Predicting the Test set results
 y_pred = Test_Classifier.predict(X_test)
-end=time.time()
+end1=time.time()
 # Making the Confusion Matrix
 from sklearn.metrics import confusion_matrix, accuracy_score
 cm = confusion_matrix(y_test, y_pred)
@@ -46,6 +48,7 @@ from sklearn.metrics import classification_report
 print(classification_report(y_test, y_pred))
 print("The accuracy of the model is  {} %".format(str(round(accuracy_score(y_test,y_pred),4)*100)))
 print(end-start)
+print(end1-start1)
 print(start)
 
 
